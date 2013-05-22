6513b93bfac4793d9296a7fffd7a4e57b0252ae8
Etienne Dysli Metref <etienne@ds3global.com>
2013-05-22 14:49:22 +0800

    Added demo DAO class

---
+++ src/main/java/ed/demo/c3p0/DemoEntityDAOImpl.java
@@ -0,0 +1,40 @@
+package ed.demo.c3p0;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.inject.Inject;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.jdbc.Work;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@Repository
+public class DemoEntityDAOImpl {
+
+	@Inject
+	private SessionFactory sessionFactory;
+
+	public DemoEntityDAOImpl() {
+	}
+
+	public DemoEntityDAOImpl(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+	
+	private Session getSession() {
+		return sessionFactory.getCurrentSession();
+	}
+	
+	private void setSchema(final String schemaName) {
+		getSession().doWork(new Work() {
+		    @Override
+		    public void execute(Connection connection) throws SQLException {
+		    	connection.setSchema(schemaName);
+		    }
+		});
+	}
+}
