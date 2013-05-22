47358ad9cbbfa42029be45a5f0bfd34808fde096
Etienne Dysli Metref <etienne@ds3global.com>
2013-05-22 15:36:24 +0800

    Implemented test case to trigger AbstractMethodError exception

--- src/test/java/ed/demo/c3p0/IntegrationTest.java
+++ src/test/java/ed/demo/c3p0/IntegrationTest.java
@@ -2,6 +2,10 @@ package ed.demo.c3p0;
 
 import static org.junit.Assert.*;
 
+import java.util.Date;
+
+import javax.inject.Inject;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
@@ -11,8 +15,14 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 public class IntegrationTest {
 
+	@Inject
+	private DemoEntityDAO dao;
+
 	@Test
-	public void test() {
-		fail("Not yet implemented"); // TODO
+	public void createEntityShouldNotThrowException() {
+		DemoEntity demoEntity = new DemoEntity();
+		demoEntity.setId(0);
+		demoEntity.setLastModifiedDate(new Date());
+		dao.createDemoEntityInAllSchemas(demoEntity);
 	}
 }
