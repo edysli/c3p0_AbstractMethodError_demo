120e77ce77b6a6dc633c4bc3fb0cdf23513e6a07
Etienne Dysli Metref <etienne@ds3global.com>
2013-05-22 14:55:55 +0800

    Added empty integration test

---
+++ src/test/java/ed/demo/c3p0/IntegrationTest.java
@@ -0,0 +1,18 @@
+package ed.demo.c3p0;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@ContextConfiguration(classes = ApplicationConfiguration.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+public class IntegrationTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented"); // TODO
+	}
+}
